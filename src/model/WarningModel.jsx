5b0f5367f8e4a55b51c88f27bd8319cfb278e0eb
MalvikaS1211 <malvikasrivastava.nadcab@gmail.com>
2025-03-19 13:56:05 +0530

    goa offer modal

--- src/model/WarningModel.jsx
+++ src/model/WarningModel.jsx
@@ -1,87 +1,69 @@
 import React from "react";
-import { IoWarning } from "react-icons/io5";
+import { IoClose } from "react-icons/io5"; // Importing a close icon
+import GOAOFFER from "../assets/Images/GOAOFFER.jpg";
 
 export default function WarningModel({ closeModal, showWarningModal }) {
+  if (!showWarningModal) return null; // Hide modal when not needed
+
   return (
-    <>
+    <div
+      className="remodal-overlay remodal-is-opened"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "rgba(0, 0, 0, 0.6)", // Dark background
+        zIndex: 1000,
+      }}
+    >
       <div
-        className="remodal-overlay remodal-is-opened"
-        style={{ display: "block" }}
+        className="remodal remodal-is-initialized remodal-is-opened"
+        style={{
+          position: "relative",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "white",
+          borderRadius: "10px",
+          overflow: "hidden",
+        }}
       >
-        <div>
-          <div
-            className="modal fade show"
-            id="exampleModalCenter"
-            tabIndex="-1"
-            role="dialog"
-            aria-labelledby="exampleModalCenterTitle"
-            aria-hidden={!showWarningModal}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <div
-              data-remodal-id="warning"
-              className="remodal remodal-is-initialized remodal-is-opened"
-              tabIndex="-1"
-            >
-              <button
-                // data-remodal-action="close"
-                className="remodal-close"
-                onClick={closeModal}
-              ></button>
-              <h1>Warning: Beware of Fake Websites</h1>
-              <div className="modaldesc">
-                <p>
-                  "It has come to our attention that a fraudulent website has
-                  been created, copying our content and design. Please be aware
-                  that this website is not affiliated with us and is attempting
-                  to scam users."
-                </p>
+        {/* Close button positioned top-right over the image */}
+        <button
+          onClick={closeModal}
+          style={{
+            position: "absolute",
+            top: "10px",
+            right: "10px",
+            background: "rgba(255, 255, 255, 0.8)",
+            border: "none",
+            cursor: "pointer",
+            padding: "5px",
+            borderRadius: "50%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <IoClose size={24} color="black" />
+        </button>
 
-                <br />
-                <br />
-
-                <b className="gt">
-                  <IoWarning size={25} /> IMPORTANT!
-                </b>
-                <ul>
-                  <li>
-                    Our official website is{" "}
-                    <a href="https://forexfusion.online/" className="gt">
-                      https://forexfusion.online
-                    </a>
-                  </li>
-                  <li>We have not launched any new websites or platforms.</li>
-                  <li>
-                    Do not provide personal or financial information to any
-                    unauthorized websites.
-                  </li>
-                </ul>
-
-                <h3>How to Identify the Fake Website:</h3>
-                <ul>
-                  <li>
-                    - Check the URL carefully. Our official website URL is{" "}
-                    <a href="https://forexfusion.online/" className="gt">
-                      https://forexfusion.online
-                    </a>
-                  </li>
-                  <li>
-                    Look for SSL certification (https) and verify our website's
-                    security.
-                  </li>
-                  <li>
-                    Be cautious of websites with similar design and content.
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+        {/* Image */}
+        <img
+          src={GOAOFFER}
+          alt="Warning"
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+            display: "block",
+          }}
+        />
       </div>
-    </>
+    </div>
   );
 }
